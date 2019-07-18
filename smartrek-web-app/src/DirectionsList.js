0de4d998e333edf89048976b3833e9827b489e8a
David Herszenhaut <dherszenhaut@anodot.com>
2019-07-18 14:40:02 +0300

    Frontend now makes a true api call and properly displays actual data

--- smartrek-web-app/src/DirectionsList.js
+++ smartrek-web-app/src/DirectionsList.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 export default class DirectionsList extends React.Component {
 
-  state = {
-    directions: [
-      'step1',
-      'step2',
-      'step3',
-    ],
-  };
+  // state = {
+  //   directions: [
+  //     'step1',
+  //     'step2',
+  //     'step3',
+  //   ],
+  // };
 
   renderDirections = () => {
     if (this.props.directions) {
       const directions = [];
       this.props.directions.forEach(function(step) {
-        console.log(step);
         directions.push(<li key={step}><b>{step[1]}</b>: {step[0]}</li>);
       });
       return directions;
